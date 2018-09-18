dc08ebcd674e5197aaf8d10e1db93696b4806c97
Nadia Bajim <nadiabajim1203@gmail.com>
2018-09-18 11:23:57 +0200

    Add Comment

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,11 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello world
+ * Author:			Nadia Bajim
+ * ----------------------------------------------------------
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
